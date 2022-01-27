7009a8c4a2fd548c96b4c0432da54cb1950443b3
huulamnguyen <liamnguyen.swe@gmail.com>
2022-01-27 14:38:03 -0500

    styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import {NavLink} from "react-router-dom";
 function NavBar(){
     return (
         <div className="categories">
+            <NavLink id="logo" to="/products">Liam&Eve Marketplace</NavLink>
             <NavLink to="/products">All Products</NavLink>
             <NavLink to="/products/categories">Catalog</NavLink>
             <NavLink to="/products/categories/electronics">Electronics</NavLink>
